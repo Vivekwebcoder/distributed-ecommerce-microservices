dc2fdfa115297996592665a1149666f55fe216bd
ponkavyaponboopathiraj <ponkavya2002@gmail.com>
2026-07-29 13:46:55 +0530

    Implemented Auth Service

---
+++ e-commerce_backend/src/main/java/ecart/ecommerce/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package ecart.ecommerce.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
